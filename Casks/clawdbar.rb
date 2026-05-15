1bb0c950bf6c38ec6b786c60e78ecd80ae475304
Vinicius Raupp <rauppv@MacBook-Pro-de-Vinicius.local>
2026-05-14 21:50:24 -0300

    Bump clawdbar cask sha256 (resource-bundle refactor build)

--- Casks/clawdbar.rb
+++ Casks/clawdbar.rb
@@ -1,6 +1,6 @@
 cask "clawdbar" do
   version "0.1.0"
-  sha256 ""
+  sha256 "5191e742809dea315de9a367975cd97670133d18a4cca7aea9e6d8d987ae18ca"
 
   url "https://github.com/rauppvj/clawdbar/releases/download/v#{version}/ClawdBar-#{version}.dmg"
   name "ClawdBar"
